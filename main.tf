faf7dae3a2822cf82d9ffad6c66cc088e9c0abaa
ravichandra2021 <85986941+ravichandra2021@users.noreply.github.com>
2024-02-02 13:24:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_subnet" "public_subnet_1" {
 # Create a public subnet 2
 resource "aws_subnet" "public_subnet_2" {
   vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 }
