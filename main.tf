2f95ae4fe7375757ae1a8fc7ce3200a04fd47893
ravichandra2021 <85986941+ravichandra2021@users.noreply.github.com>
2024-02-02 13:30:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,35 +12,35 @@ provider "aws" {
 }
 
 # Create a VPC
-resource "aws_vpc" "my_vpc" {
+resource "aws_vpc" "my_newvpc" {
   cidr_block = "10.0.0.0/16"
 }
 
 # Create a public subnet 1
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id                  = aws_vpc.my_vpc.id
+  vpc_id                  = aws_vpc.my_newvpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
 # Create a public subnet 2
 resource "aws_subnet" "public_subnet_2" {
-  vpc_id                  = aws_vpc.my_vpc.id
+  vpc_id                  = aws_vpc.my_newvpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 }
 # Create an Internet Gateway 1
 resource "aws_internet_gateway" "my_internet_gateway_1" {
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = aws_vpc.my_newvpc.id
 }
 # Create an Internet Gateway 2
 resource "aws_internet_gateway" "my_internet_gateway_2" {
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = aws_vpc.my_newvpc.id
 }
 # Create a route table for the public subnet 1
 resource "aws_route_table" "public_route_table_1" {
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = aws_vpc.my_newvpc.id
 }
 
 # Associate the public subnet with the public route table 1
@@ -74,7 +74,7 @@ resource "aws_route" "internet_route_2" {
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow inbound SSH traffic"
-  vpc_id      = aws_vpc.my_vpc.id
+  vpc_id      = aws_vpc.my_newvpc.id
 
   ingress {
     from_port   = 22
